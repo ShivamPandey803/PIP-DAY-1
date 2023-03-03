bd84ff86cebb3c34c5f29717ce396e062f643c80
Shivam Pandey <shivampand08@gmail.com>
2023-03-03 12:49:13 +0530

    Adding Readme file for my dynamic array explaination.

---
+++ dynamicArray.js
@@ -0,0 +1,34 @@
+const numArrays = 6;
+const arrays = [];
+
+for (let i = 0; i<numArrays; i++)
+{
+    arrays.push([])
+}
+
+
+let currentArrayIndex = 0;
+
+const elements = ['a','b','c','d','e','f',1,2,3,4,5,7,8,9,10,];
+
+for (let i = 0; i< elements.length; i++){
+    const currentArray = arrays[currentArrayIndex];
+    currentArray.push(elements[i]);
+    currentArrayIndex = (currentArrayIndex +1) % numArrays
+}
+
+// Printing the final contents of the arrays
+
+for(let i = 0; i<arrays.length;i++){
+    console.log(`Array ${i+1}:${arrays[i]}`);
+
+}
+
+// output of the program
+// Array 1:a,1,8
+// Array 2:b,2,9
+// Array 3:c,3,10
+// Array 4:d,4
+// Array 5:e,5
+// Array 6:f,7
+
