6f4f24ff59c94c896bdf5014ec84138259d72d26
Shivam Pandey <shivampand08@gmail.com>
2023-03-14 14:46:14 +0530

    creating a todo list app using vue js.

---
+++ todo-list-app/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="newTodoText">
+      <button>Add Todo</button>
+    </form>
+    <div v-if="todos.length === 0">No todos yet!</div>
+    <div v-else>
+      <todo-item
+        v-for="(todo, index) in todos"
+        :key="index"
+        :todo="todo"
+        @delete-todo="deleteTodo(index)"
+      ></todo-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue';
+
+export default {
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+      newTodoText: '',
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      console.log(111)
+
+      if (this.newTodoText.trim() !== '') {
+        this.todos.push({
+          text: this.newTodoText.trim(),
+          completed: false,
+        });
+        this.newTodoText = '';
+      }
+    },
+    deleteTodo(index) {
+      console.log(222)
+
+      this.todos.splice(index, 1);
+    },
+  },
+};
+</script>
---
+++ todo-list-app/src/components/TodoItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="checkbox" v-model="completed">
+    <span :class="{ completed: completed }">{{ text }}</span>
+    <button @click="$emit('delete-todo')">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: Object,
+  },
+  data() {
+    return {
+      completed: this.todo.completed,
+      text: this.todo.text,
+    };
+  },
+};
+</script>
+
+<style>
+.completed {
+  text-decoration: line-through;
+}
+</style>
