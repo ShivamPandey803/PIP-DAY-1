77261fb11db44a0e88c76aa8ecdd7422bd06521e
Shivam Pandey <shivampand08@gmail.com>
2023-03-21 00:40:03 +0530

    Added css to my todo list app

--- todo-list-app/src/App.vue
+++ todo-list-app/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
-    <h1>Todo List</h1>
-    <form @submit.prevent="addTodo">
-      <input type="text" v-model="newTodoText" placeholder="Enter a todo">
-      <input type="text" v-model="newTodoDesc" placeholder="Enter a description">
-      <button>Add Todo</button>
+  <div class="app">
+    <h1 class="title">Todo List</h1>
+    <form @submit.prevent="addTodo" class="form">
+      <input type="text" v-model="newTodoText" placeholder="Enter a todo" class="input-text">
+      <input type="text" v-model="newTodoDesc" placeholder="Enter a description" class="input-text">
+      <button class="btn-add">Add Todo</button>
     </form>
-    <div v-if="todos.length === 0">No todos yet!</div>
+    <div v-if="todos.length === 0" class="no-todos">No todos yet!</div>
     <div v-else>
       <todo-item
         v-for="(todo) in todos"
@@ -60,3 +60,56 @@ export default {
   },
 };
 </script>
+
+<style>
+.app {
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.title {
+  background-color: #f44336;
+  padding: 30px 40px;
+  color: white;
+  text-align: center;
+
+}
+
+.form {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+}
+
+.input-text {
+  flex-grow: 1;
+  padding: 0.5rem;
+  border-radius: 5px;
+  border: none;
+  font-size: 1rem;
+  margin-right: 0.5rem;
+}
+
+.btn-add {
+  padding: 2px;
+    margin: 0;
+    max-width: 100%;
+    overflow-x: hidden;
+    position: relative;
+    display: block;
+    background-color: #3cb371;
+    border-radius: 5px;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
+
+.no-todos {
+  text-align: center;
+  font-size: 1rem;
+  color: gray;
+  margin-top: 1rem;
+}
+</style>
+
--- todo-list-app/src/components/TodoItem.vue
+++ todo-list-app/src/components/TodoItem.vue
@@ -1,17 +1,16 @@
-
 <template>
-  <div>
+  <div class="todo-item">
     <div v-if="!isEditing">
-      <input type="checkbox" v-model="completed" @change="$emit('update-todo', {id, todo: {text, description, completed}})">
-      <span :class="{ completed: completed }">{{ text }}</span>
-      <span v-if="description">- {{ description }}</span>
-      <button @click="$emit('delete-todo', id)">X</button>
-      <button @click="isEditing = true">Edit</button>
+      <input type="checkbox" v-model="completed" @change="$emit('update-todo', {id, todo: {text, description, completed}})" class="checkbox">
+      <span :class="{ completed: completed }" class="text">{{ text }}</span>
+      <span v-if="description" class="description">- {{ description }}</span>
+      <button @click="$emit('delete-todo', id)" class="btn-delete">X</button>
+      <button @click="isEditing = true" class="btn-edit"> <span>&#9999;</span> </button>
     </div>
     <div v-else>
-      <input type="text" v-model="text">
-      <input type="text" v-model="description">
-      <button @click="isEditing = false; $emit('update-todo', {id, todo: {text, description, completed}})">Save</button>
+      <input type="text" v-model="text" class="input-text">
+      <input type="text" v-model="description" class="input-text">
+      <button @click="isEditing = false; $emit('update-todo', {id, todo: {text, description, completed}})" class="btn-save">Save</button>
     </div>
   </div>
 </template>
@@ -33,79 +32,75 @@ export default {
 };
 </script>
 <style>
-h1 {
-    font-size: 2.5rem;
-    margin-bottom: 1.5rem;
-    text-align: center;
-  }
-  
-  form {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 1.5rem;
-  }
-  
-  input[type="text"] {
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    font-size: 1rem;
-    margin-right: 1rem;
-    padding: 0.5rem;
-    width: 50%;
-  }
-  
-  button {
-    background-color: #4CAF50;
-    border: none;
-    border-radius: 4px;
-    color: white;
-    cursor: pointer;
-    font-size: 1rem;
-    padding: 0.5rem;
-  }
-  
-  button:hover {
-    background-color: #3e8e41;
-  }
-  
-  .todo-item {
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 0.5rem;
-  }
-  
-  .todo-item span {
-    font-size: 1.2rem;
-    margin-right: 1rem;
-  }
-  
-  .todo-item button {
-    background-color: #f44336;
-    border: none;
-    border-radius: 4px;
-    color: white;
-    cursor: pointer;
-    font-size: 1rem;
-    margin-right: 1rem;
-    padding: 0.5rem;
-  }
-  
-  .todo-item button:hover {
-    background-color: #d32f2f;
-  }
-  
-  .completed {
-    text-decoration: line-through;
-  }
-  .description {
-  font-size: 0.8rem;
+* {
+  box-sizing: border-box;
+}
+
+.todo-item {
+  cursor: pointer;
+  position: relative;
+  padding: 12px 8px 12px 40px;
+  list-style-type: none;
+  background: #eee;
+  font-size: 25px;
+  transition: 0.2s;
+}
+.todo-item:nth-child(odd) {
+  background: #f9f9f9;
+}
+.todo-item:hover {
+  background: #ddd;
+}
+
+.checkbox {
+  margin-right: 0.5rem;
+}
+
+.text {
+  flex-grow: 1;
+  font-size: 1.5rem;
+}
+.completed {
+  background: #888;
+  color: #fff;
+  text-decoration: line-through;
+}
+
+.description {
+  font-size: 1rem;
   color: gray;
   margin-top: 0.5rem;
 }
+.btn-delete{
+  position: absolute;
+    right: 0;
+    cursor: pointer;
+    border-radius: 5px;
+    border: none;
+}
+.btn-delete:hover {
+  background-color: #f44336;
+  color: white;
+}
+.btn-edit{
+
+  cursor: pointer;
+    border-radius: 5px;
+    border: none;
+}
+.btn-edit:hover{
+  background-color: #79f704;
+  color: white;
+}
+.btn-save{
+  cursor: pointer;
+    border-radius: 5px;
+    border: none;
+}
+.btn-save:hover{
+  background-color: #79f704;
+  color: white;
+
+}
+
 </style>
-
-
-
-
-
